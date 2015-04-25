71bd680ab37c37f779ea685c8f4253b1905b8343
Gilmar Lira <gilmar.lira@me.com>
2015-04-25 15:25:37 -0700

    Adds lots of SASS improvements.

--- www/js/interactions.js
+++ www/js/interactions.js
@@ -1,51 +1,46 @@
 
+// interactions
+// ////////////////////////////////////////////////////
+
+var $current, $line1, $line2, $line3, $line4, $author, count=0, cubes = 48;
+
 $(document).ready(function(){
 	init();
-	
-    $(".face").hover(function(){
-        // $(this).parent().find(".face").css('opacity','.5');
-        $(this).toggleClass("hover");
-    });
 
-    $(".face").click(function(){
-        if($(this).is(".off")){
-            $(this).toggleClass("off", false);
-        } else {
-            $(this).toggleClass("off", true);
-        }
-    });
-    
-    $(".fill").click(function(){
-    	$(".face").toggleClass("off", false);
-    });
-    
-    $(".clear").click(function(){
-    	$(".face").toggleClass("off", true);
-    });
-    
-    $(".write-dialog").click(function(){
-    	$(".form-container").toggleClass("active");
-    });
+	// quote cycle
+	$(".next").click(function() {
+  	if(count == quotes.length-1) {
+  		count=0;
+  	} else {
+  		count++;
+  	}
 
-    $(".next").click(function(){
-    	if(count==quotes.length-1){
-    		count=0;
-    	} else{
-    		count++;
-    	}
-    	$line1=quotes[count].line1;
-    	$line2=quotes[count].line2;
-    	$line3=quotes[count].line3;
-    	$line4=quotes[count].line4;
-    	$author=quotes[count].author;
-    	write();
-    });
-    
-    $(".cancel").click(function(){
-    	$(".form-container").toggleClass("active");
-    });
-    
-    $(".write").click(function(){
+		write();
+  });
+
+
+	// box interations
+  $(".face").hover(function(){ $(this).toggleClass("hover"); });
+  $(".face").click(function() {
+		var $t = $(this);
+    if($t.is(".off")) {
+      $t.toggleClass("off", false);
+    } else {
+      $t.toggleClass("off", true);
+    }
+  });
+
+
+	// Bottom controls
+  $(".fill").click(function() { $(".face").toggleClass("off", false); });
+  $(".clear").click(function() { $(".face").toggleClass("off", true); });
+  $(".write-dialog").click(function() { $(".form-container").toggleClass("active"); });
+
+
+  // write quote
+  $(".cancel").click(function() { $(".form-container").toggleClass("active"); });
+
+  $(".write").click(function() {
 		$line1=$("input").eq(0).val().toUpperCase();
 		$line2=$("input").eq(1).val().toUpperCase();
 		$line3=$("input").eq(2).val().toUpperCase();
@@ -53,28 +48,36 @@ $(document).ready(function(){
 		$author=$("input[name='author']").eq(0).val().toUpperCase();
 		$(".form-container").toggleClass("active");
 		write();
-    	
-    });
+  });
 
 });
 
-var $current, $line1, $line2, $line3, $line4, $author, count=0;
+
+
+
 
 function init(){
-	$line1=quotes[count].line1;
-	$line2=quotes[count].line2;
-	$line3=quotes[count].line3;
-	$line4=quotes[count].line4;
-	$author=quotes[count].author;
-	write();
+	$cube_container = $(".cube-container");
+
+	for(var c = 0; c < cubes; c++) {
+		var $c = $('<div class="cube"> <div class="face off top-close"></div> <div class="face off top-far"></div> <div class="face off right-bottom"></div> <div class="face off right-top"></div> <div class="face off bottom-close"></div> <div class="face off bottom-far"></div> <div class="face off left-bottom"></div> <div class="face off left-top"></div> <div class="face off middle-bottom"></div> <div class="face off middle-top"></div> <div class="face off cross"></div> </div>');
+		$cube_container.append($c);
+	}
+
 	resize();
+	write();
 }
 
+
+
 function resize(){
-	$cube_width=$(".cube").width();
-	$(".cube").height(2*$cube_width);
-	// $cube_height=$(".cube").height();
-	// $(".cube").width($cube_height*.5);
+	$c = $(".cube");
+	$cube_width = $c.width();
+	$c.height(2 * $cube_width);
+
+	$delta = $(window).height() - $(".cube-container").height();
+	$(".cube-container").css("top", $delta * 0.4);
+
 }
 
 var delay = (function(){
@@ -93,15 +96,21 @@ $(window).resize(function () {
 });
 
 function write(){
-	if($author){ 	$(".by-line").text($author);	}	 
+	$line1 = quotes[count].line1;
+	$line2 = quotes[count].line2;
+	$line3 = quotes[count].line3;
+	$line4 = quotes[count].line4;
+	$author = quotes[count].author;
+
+	if($author){ 	$(".by-line").text($author);	}
 	else { 			$(".by-line").text("UNKNOWN");	}
 
 	for(var l=0; l<4; l++){
-		if(l==0){ $current=$line1; }
+		if(l===0){ $current=$line1; }
 		if(l==1){ $current=$line2; }
 		if(l==2){ $current=$line3; }
-		if(l==3){ $current=$line4; }	
-	
+		if(l==3){ $current=$line4; }
+
 		if($current){
 			for(var i=0; i<$current.length; i++){
 				var $cube=$(".cube").eq((12*l)+i);
@@ -117,7 +126,7 @@ function write(){
 					$cube.find(".middle-top").toggleClass("off", true);
 					$cube.find(".middle-bottom").toggleClass("off", true);
 					$cube.find(".cross").toggleClass("off", false);
-				} 
+				}
 				else if($current[i]=='B'){
 					$cube.find(".top-close").toggleClass("off", false);
 					$cube.find(".top-far").toggleClass("off", false);
@@ -447,16 +456,15 @@ function write(){
 					$cube.find(".face").toggleClass("off", true);
 				}
 			}
-		
+
 			if($current.length<12){
-				for(var i=$current.length; i<12; i++){
-					var $cube=$(".cube").eq((l*12)+i);
-					$cube.find(".face").toggleClass("off", true);
+				for(var j=$current.length; j<12; j++){
+					var $scube=$(".cube").eq((l*12)+j);
+					$scube.find(".face").toggleClass("off", true);
 				}
 			}
 		}
 	}
-
 }
 
 
@@ -466,47 +474,46 @@ function write(){
 // ////////////////////////////////////////////////////
 
 var quotes = [
-    {   
+    {
         line1: 'COMPUTERS',
         line2: 'ARE USELESS',
         line3: 'THEY ONLY',
         line4: 'GIVE ANSWERS',
         author: 'Pablo Picasso'
-    }, 
-    {   
+    },
+    {
         line1: 'SO MUCH',
         line2: 'TECHNOLOGY',
         line3: 'SO LITTLE',
         line4: 'TALENT',
         author: 'Vernor Vinge'
     },
-    {   
+    {
         line1: 'HARDWARE IS',
         line2: 'THE PART OF',
         line3: 'THE COMPUTER',
         line4: 'WE CAN KICK',
         author: 'Jeff Pesis'
-    }, 
-    {   
+    },
+    {
         line1: 'TECHNOLOGY',
         line2: 'IS A GOOD',
         line3: 'SERVANT BUT',
         line4: 'A BAD MASTER',
         author: 'Christian Lous Lange'
-    }, 
-    {   
+    },
+    {
         line1: 'THE HUMAN',
         line2: 'SPIRIT MUST',
         line3: 'PREVAIL OVER',
         line4: 'TECHNOLOGY',
         author: 'Albert Einstein'
-    }, 
-    {   
+    },
+    {
         line1: 'ADVANCED',
         line2: 'TECHNOLOGY',
         line3: 'IS EQUAL',
         line4: ' TO MAGIC',
         author: 'Sir Arthur C. Clarke'
-    } 
+    }
 ];
-
